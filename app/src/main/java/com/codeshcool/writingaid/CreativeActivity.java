c7a980fddcc579c7118df597d918549c26247802
davina <davina.kuhnel@gmail.com>
2018-06-03 15:46:44 +0100

    added to the setup of the Creative Activity

--- app/src/main/java/com/codeshcool/writingaid/CreativeActivity.java
+++ app/src/main/java/com/codeshcool/writingaid/CreativeActivity.java
@@ -5,26 +5,45 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class CreativeActivity extends AppCompatActivity {
+    Creative creative;
+    TextView analysis;
+    EditText insert;
+    EditText lexicalField;
 
-    TextView insert;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_creative);
 
-        insert = findViewById(R.id.creativeTextViewID);
+        analysis = findViewById(R.id.creativeTextViewID);
 
-        insert.setMovementMethod(new ScrollingMovementMethod());
+
+
 
         Intent intent = getIntent();
 //        Creative creative = (Creative)intent.getSerializableExtra("creative");
 
 //        Log.d("Essay Activity: ", creative.getDate());
 
+
+    }
+
+    public void onButtonCreativeAnalysisClick(View view){
+        String userInput = insert.getText().toString();
+        double result = essay.readabilityScore(userInput);
+        String resultString = Double.toString(result);
+        analysis.setText(resultString);
+
+    }
+
+    public void onButtonLexicalFieldClick(View view){
+       String userInput = lexicalField.getText().toString();
 
     }
 
